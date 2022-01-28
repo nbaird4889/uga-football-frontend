a76b6e1c6d98bec663a64f3656eed1e0d480147c
Nicole Baird <nbaird44@gmail.com>
2022-01-28 09:58:40 -0500

    addition of poster to homepage video

--- src/components/Video.js
+++ src/components/Video.js
@@ -4,14 +4,14 @@ function Video(props) {
     return (
         <div className="video">
             <video id="background-video" autoPlay loop muted>
-                <source src="https://res.cloudinary.com/dhbumhhh1/video/upload/v1643297705/Football/football-video_jnhayw.mp4" type="video/mp4" />
+                <source src="https://res.cloudinary.com/dhbumhhh1/video/upload/v1643297705/Football/football-video_jnhayw.mp4" type="video/mp4" poster="https://res.cloudinary.com/dhbumhhh1/image/upload/v1643381797/Football/Screen_Shot_2022-01-28_at_9.55.15_AM-min_tl9wn5.png" />
             </video>
 
             <div className="introduction">
             <h2 className="intro-headline">UNIVERSITY OF GEORGIA FOOTBALL</h2>
             <h3 className="intro-subhead">2021 NATIONAL CHAMPIONS</h3>
             <p className="intro-text">
-                Check out the legends of UGA Football who all paved the way.
+                Check out the legends of UGA Football who paved the way.
             </p>
             <Link to="/players">
             <button className="intro-btn">LET'S GO</button>
